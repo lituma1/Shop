2e4893a1cbfc19dc410c674674ea6339c1dbb43b
Michal Stasiak <Stasiu@Air-Michal.home>
2016-12-10 21:16:42 +0100

    some html code

--- index.php
+++ index.php
@@ -1,8 +1,40 @@
-<?php
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Best shope ever!</title>
+        <link rel="stylesheet" type="text/css" href="../css/style.css"   
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="sideBySide">
+                <h1>Produkty:</h1>
+                <ul>
+                    <li><a href="#">Lorem ipsum</a></li>
+                    <li><a href="#">Lorem ipsum</a></li>
+                    <li><a href="#">Lorem ipsum</a></li>
+                    <li><a href="#">Lorem ipsum</a></li>
+                    <li><a href="#">Lorem ipsum</a></li>
+                </ul>
+            </div>
+            <div class="sideBySide" style="text-align: center">
+                
+                <form action="#" method="POST">
+                    <input type="text" name="email" placeholder="email"><br>
+                    <input type="text" name="password" placeholder="haslo"><br>
+                    <input type="radio" name="typeOfUser" value="client">Klient<br>
+                    <input type="radio" name="typeOfUser" value="admin">Admin<br>
+                    <input type="submit" name="submit" value="Wyslij"> 
+                </form>
+                <h2>Nowy klient?</h2>
+                <button><a href="registration.php">zarejestruj się!</a></button>
+            </div>
+        </div>
+        
+        <div class="wrapper">
+            <h1>Tu będzie karuzela</h1>
+        </div>
+    </body>
+</html>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
---
+++ web/registration.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Rejestracja</title>
+        <link rel="stylesheet" type="text/css" href="../css/style.css"
+    </head>
+    <body>
+        <div style="text-align: center">
+            <br><br>
+            <h1>Rejestracja</h1>
+            <form action="#" method="POST">
+                <input type="text" name="name" placeholder="Imię"><br><br>
+                <input type="text" name="surname" placeholder="Nazwisko"><br><br>
+                <input type="text" name="email" placeholder="email"><br><br>
+                <input type="text" name="password" placeholder="hasło"><br><br>
+                <input type="text" name="shippingAddress" placeholder="adres do wysyłki"><br><br>
+                <input type="submit" name="submit" value="zarejstruj się!"
+            </form>
+        </div>
+        <div class="wrapper"></div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
